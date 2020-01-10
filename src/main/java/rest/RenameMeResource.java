0a349f80efff3c0d814a1e249e407a0e14fdd5d7
Epiko1994 <cph-jj442@cphbusiness.dk>
2020-01-10 23:54:58 +0100

    Sidste commit Fredag

--- src/main/java/rest/RenameMeResource.java
+++ src/main/java/rest/RenameMeResource.java
@@ -6,10 +6,11 @@ import entities.Person;
 import utils.EMF_Creator;
 import facades.FacadeExample;
 import javax.persistence.EntityManagerFactory;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 //Todo Remove or change relevant parts before ACTUAL use
@@ -41,5 +42,20 @@ public class RenameMeResource {
     public String getAllPersons() {
         List<Person> persons = FACADE.getAllPersons();
         return GSON.toJson(persons);
+    }
+
+    @Path("name/{firstName}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getPersonsByFirstName(Person entity, @PathParam("firstName") String firstName) throws NotFoundException, ParseException, IOException {
+        List<Person> persons = FACADE.getAllPersons();
+        List<Person> sortedPersonsByFirstName = new ArrayList<>();
+        for (Person p : persons) {
+            if (p.getFirstName().equalsIgnoreCase(firstName)) {
+                sortedPersonsByFirstName.add(p);
+            }
+        }
+        if(sortedPersonsByFirstName.isEmpty()){throw new NotFoundException("Der blev ikke fundet nogen person med dette navn");}
+            return GSON.toJson(sortedPersonsByFirstName);
     }
 }
